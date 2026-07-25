d82ad786719eaccc9a2efab6ff4dfb1d61a0b1c4
Jashan Bhyan <bhyanjashan@gmail.com>
2026-07-25 11:06:54 +0530

    Time: 7 ms (85.3%), Space: 9.3 MB (78.23%) - LeetHub - Time: O(n^2), Space: O(1)

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.size();
+
+        if (n <= 1)
+            return s;
+
+        int start = 0;
+        int maxLen = 1;
+
+        for (int i = 0; i < n; i++) {
+            int left = i;
+            int right = i;
+
+            while (left >= 0 && right < n && s[left] == s[right]) {
+                if (right - left + 1 > maxLen) {
+                    start = left;
+                    maxLen = right - left + 1;
+                }
+                left--;
+                right++;
+            }
+            left = i;
+            right = i + 1;
+
+            while (left >= 0 && right < n && s[left] == s[right]) {
+                if (right - left + 1 > maxLen) {
+                    start = left;
+                    maxLen = right - left + 1;
+                }
+                left--;
+                right++;
+            }
+        }
+
+        return s.substr(start, maxLen);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
