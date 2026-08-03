0b05030d650d31601173d0f2e11e46e69d85f786
Jashan Bhyan <bhyanjashan@gmail.com>
2026-08-03 14:32:12 +0530

    Time: 0 ms (100%), Space: 59.1 MB (43.64%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node/0117-populating-next-right-pointers-in-each-node.cpp
+++ 0117-populating-next-right-pointers-in-each-node/0117-populating-next-right-pointers-in-each-node.cpp
@@ -1,23 +1,48 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
 class Solution {
-public:
-    Node* connect(Node* root) {
-        if(!root) return nullptr;
-        queue<Node*> q;
-        q.push(root);        
-        while(size(q)) {
-            Node* rightNode = nullptr;                    // set rightNode to null initially
-            for(int i = size(q); i; i--) {                // traversing each level
-                auto cur = q.front(); q.pop();            // pop a node from current level and,
-                cur -> next = rightNode;                  // set its next pointer to rightNode
-                rightNode = cur;                          // update rightNode as cur for next iteration
-                if(cur -> right)                          // if a child exists
-                    q.push(cur -> right),                 // IMP: push right first to do right-to-left BFS
-                    q.push(cur -> left);                  // then push left
+    public Node connect(Node root) {
+        if(root==null)return root;
+        Queue<Node>queue=new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int len=queue.size();
+            Node prev=null;
+            for(int i=0;i<len;i++){
+                Node cur=queue.poll();
+                if(prev!=null){
+                    prev.next=cur;
+                }
+                prev=cur;
+                if(cur.left!=null)queue.offer(cur.left);
+                if(cur.right!=null)queue.offer(cur.right);
             }
+            prev.next=null;
         }
         return root;
     }
-};
+}
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
