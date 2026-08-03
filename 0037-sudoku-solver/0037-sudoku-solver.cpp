bb96febeb368ad3bc63c5ae64bd38828fce43355
Jashan Bhyan <bhyanjashan@gmail.com>
2026-08-03 14:26:19 +0530

    Time: 228 ms (80.42%), Space: 8.9 MB (9.94%) - LeetHub - Time: O(9^(n*n)), Space: O(n*n)

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    bool ispossible(int x, int y, vector<vector<char>>& board, char ch) {
+        // row check
+        for (int i = 0; i < 9; i++) {
+            if (board[x][i] == ch) {
+                return false;
+            }
+        }
+        // column check
+        for (int i = 0; i < 9; i++) {
+            if (board[i][y] == ch) {
+                return false;
+            }
+        }
+        // box check
+        int sx = (x / 3) * 3;
+        int sy = (y / 3) * 3;
+        for (int i = sx; i < sx + 3; i++) {
+            for (int j = sy; j < sy + 3; j++) {
+                if (board[i][j] == ch) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    bool ans = false;
+    void F(int i, int j, vector<vector<char>>& board) {
+        if (i == 9) {
+            ans = true;
+            return;
+        }
+        if (j == 9) {
+            F(i + 1, 0, board);
+            return;
+        }
+        if (board[i][j] == '.') {
+            for (int k = 0; k < 9; k++) {
+                char ch = '1' + k;
+                if (ispossible(i, j, board, ch)) {
+                    board[i][j] = ch;
+                    F(i, j + 1, board);
+                    if (ans) {
+                        return;
+                    }
+                    board[i][j] = '.';
+                }
+            }
+        } else {
+            F(i, j + 1, board);
+        }
+        return;
+    }
+    void solveSudoku(vector<vector<char>>& board) {
+        F(0, 0, board);
+        return;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
