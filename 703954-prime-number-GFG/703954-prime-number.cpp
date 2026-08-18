42fc038bbf9c7dcd3c352d1ee5a75cbf49c68bdb
Jashan Bhyan <bhyanjashan@gmail.com>
2026-08-18 10:03:29 +0530

    Added solution - Time: O(sqrt(n)), Space: O(1)

--- 703954-prime-number-GFG/703954-prime-number.cpp
+++ 703954-prime-number-GFG/703954-prime-number.cpp
@@ -2,11 +2,18 @@ class Solution {
   public:
     bool isPrime(int n) {
         // code here
+        // Current Time Complexity: O(n) - Linear
+        // Current Space Complexity: O(1) - Constant
+        // Optimal Time Complexity: O(sqrt(n))
+        
         bool res=true;
         if(n==1){
             return false;
         }
-        for(int i=2;i<n;i++){
+        // HINT: You are checking up to n/2. While correct, you can optimize this significantly.
+        // Think about the properties of divisors: if n = a * b, one of them must be <= sqrt(n).
+        // Try changing the loop condition to i * i <= n to achieve O(sqrt(n)) complexity.
+        for(int i=2;i*i<=n;i++){
             if(n%i==0){
                 return false;
             }
@@ -14,7 +21,6 @@ class Solution {
         return true;
     }
 };
-
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
