b175a1816c8c52e3ee3703f5e14bc86244b9fae6
Jashan Bhyan <bhyanjashan@gmail.com>
2026-08-18 10:28:21 +0530

    Time: 1048 ms (42.46%), Space: 7.7 MB (55.52%) - LeetHub - Time: O(num), Space: O(1)

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int sum=1;
+        if(num==1){
+            return false;
+        }
+        for(int i=2;i<=num/2;i++){
+            if(num%i==0){
+                sum+=i;
+            }
+        }
+        if(sum==num){
+            return true;
+        }
+        return false;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
