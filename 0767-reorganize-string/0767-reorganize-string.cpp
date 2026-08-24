0a32d769701b04b86fc29db4d0470acea356c3fb
Jashan Bhyan <bhyanjashan@gmail.com>
2026-08-24 14:14:25 +0530

    Time: 0 ms (100%), Space: 8.6 MB (29.34%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -1,37 +1,43 @@
 class Solution {
 public:
     string reorganizeString(string s) {
-        unordered_map<char,int> mp;
-        for(char i:s){
-            mp[i]++;
+        vector<int> freq(26, 0);
+        for (char c : s) {
+            freq[c - 'a']++;
         }
-        priority_queue<pair<int,char>> pq;
-        for(auto i:mp){
-            pq.push({i.second,i.first});
-        }
-        string res="";
-        while(pq.size()>=2){
-            auto [freq1,char1]=pq.top();
-            pq.pop();
-            auto [freq2,char2]=pq.top();
-            pq.pop();
-            res+=char1;
-            res+=char2;
-
-            if(--freq1>0){
-                pq.push({freq1,char1});
-            }
-            if(--freq2>0){
-                pq.push({freq2,char2});
+        priority_queue<pair<int, char>> pq;
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0) {
+                pq.push({freq[i], 'a' + i});
             }
         }
-        while(!pq.empty()){
-            auto [freq,char1]=pq.top();
-            if(freq>1){
-                return "";
-            }
-            res+=char1;
+        string res = "";
+        char prev = '#';
+        while (!pq.empty()) {
+            auto [count, value] = pq.top();
             pq.pop();
+            if (value == prev) {
+                if (pq.empty()) {
+                    return "";
+                }
+                auto [count2, value2] = pq.top();
+                pq.pop();
+                res += value2;
+                prev = value2;
+                count2--;
+                if (count2 > 0) {
+                    pq.push({count2, value2});
+                }
+                pq.push({count, value});
+            }
+            else {
+                res += value;
+                prev = value;
+                count--;
+                if (count > 0) {
+                    pq.push({count, value});
+                }
+            }
         }
         return res;
     }
