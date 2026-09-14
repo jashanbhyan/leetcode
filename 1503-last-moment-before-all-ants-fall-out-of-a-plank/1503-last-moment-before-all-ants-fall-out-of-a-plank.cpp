05d69f2f69798d1228600ed7c26be1a4a867f612
Jashan Bhyan <bhyanjashan@gmail.com>
2026-09-14 14:58:14 +0530

    Time: 0 ms (100%), Space: 26.7 MB (99.79%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 1503-last-moment-before-all-ants-fall-out-of-a-plank/1503-last-moment-before-all-ants-fall-out-of-a-plank.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+        int a = 0;
+        int b = 0;
+        if (!left.empty()) {
+            a = *max_element(left.begin(), left.end());
+        }
+        if (!right.empty()) {
+            b = n - *min_element(right.begin(), right.end());
+        }
+        return max(a, b);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
